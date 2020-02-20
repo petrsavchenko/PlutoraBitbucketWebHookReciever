1ddacd40a4f3e062aee09b5334cdc44ab9b0ade7
Petr Savchenko <petr.savchenko@plutora.com>
2020-02-21 09:47:09 +1100

    Init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = 3000
+
+app.get('/', (req, res) => res.send(JSON.stringify(req.body)));
+
+app.post('/hook', (req, res) => { console.log(req.body); res.sendStatus(200)});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
