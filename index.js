46bbef21ea2d011163a94bfa17856488aac1f397
Petr Savchenko <petr.savchenko@plutora.com>
2020-02-21 10:04:10 +1100

    Added port and package

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send(JSON.stringify(req.body)));
 
