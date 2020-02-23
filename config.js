2b40c3ce53cd3e741c6c7e0211b91ec1cf98c635
Petr Savchenko <petr.savchenko@plutora.com>
2020-02-23 12:37:42 +1100

    added merged transition

--- config.js
+++ config.js
@@ -5,10 +5,20 @@ module.exports = {
     version: '0.0.0.1',
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
+    plutora: {
+        dev_branches: ['DEV-AQUAMARINE','DEV-RUBY'],
+        feature_branch_prefix: 'DEV-FEATURE-'
+    },
     jira: {
         url: 'https://testpetr.atlassian.net',
         issueRegEx: /((?<!([A-Z]{1,10})-?)[A-Z]+-\d+)/g, // official Jira Issue Reg Ex - https://confluence.atlassian.com/stashkb/integrating-with-custom-jira-issue-key-313460921.html?_ga=2.203384471.708255576.1582337364-28417654.1581479138
-        prApprovedStatusId: 21,
+        statuses: {
+            prApproved: 21,
+            merged: 11
+        },
+        qa: {
+            TEST: ['petr savchenko']
+        }
     },
     creds: {
         username: 'petr.savchenko@hotmail.com',
--- index.js
+++ index.js
@@ -7,38 +7,85 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/toPrApproved', (req, res) => {
+// Called on Pull Request Approved trigger in bitbucket
+app.post('/pr_approved', (req, res) => {
     if (!req || !req.body || !req.body.pullrequest) {
         console.log('request is invalid');
         res.status(400).send('request is invalid');
         return;
     }
+    // {pullrequest}: The approved pull request.
+    // {participants}: A list of participants part of the pull request.
+    // {title}: The name of the pull request. 
+    const { participants, title } = req.body.pullrequest;
 
-    const pullrequest = req.body.pullrequest;
-
-    if (pullrequest.participants.filter(p => p.approved).length < 1) {
+    if (participants.filter(p => p.approved).length < 1) {
         res.status(202).send('not enough approvers');
         return;
     }
 
-    const bodyData = `{
-        "transition": {
-            "id": ${config.jira.prApprovedStatusId} 
-        }
-    }`;
+    const issueId = parseIssueId(title);
 
-    const title = pullrequest.rendered.title.raw;
-    const matchesArray = title.match(config.jira.issueRegEx);
-
-    if (matchesArray.length !== 1) {
+    if (!issueId) {
         console.log('jira issue id was not found in the pull request title');
         res.status(400).send('jira issue id was not found in the pull request title');
         return;
     }
 
-    var issueId = matchesArray[0];
+    requestToJira(res, config.jira.statuses.prApproved, findQaAssignee(issueId));
+});
 
-    var options = {
+// Called on any push to repo in bitbucket
+app.post('/merged', (req, res) => {
+    if (!req || !req.body || !req.body.push) {
+        console.log('request is invalid');
+        res.status(400).send('request is invalid');
+        return;
+    }
+    // The details of the push, which includes the changes property. 
+    // {new}: An object containing information about the state of the reference after the push. 
+    // When a branch is deleted, new is null
+    // {name}: The name of the branch, tag, named branch, or bookmark.
+    // {target}: The details of the most recent commit after the push.
+    const push = req.body.push;
+
+    if (push.changes.length === 1 && push.changes[0].new 
+        && push.changes[0].new.target && push.changes[0].new.target.type === 'commit'
+        // check if it was commit to dev or feature branch
+        && (config.plutora.dev_branches.includes(push.changes[0].new.target.name) 
+            || push.changes[0].new.target.name.startsWith(config.plutora.feature_branch_prefix))
+        // check if it was 'merge' commit
+        && push.changes[0].new.target.message.startsWith('Merged in '))
+    {
+        const issueId = parseIssueId(push.changes[0].new.target.message);
+
+        if (!issueId) {
+            console.log('jira issue id was not found in the merge commit message');
+            res.status(400).send('jira issue id was not found in the merge commit message');
+            return;
+        }
+
+        requestToJira(res, config.jira.statuses.merged);
+    }
+
+    res.status(202).send('no merges have been detected');
+    return;
+});
+
+const parseIssueId = (text) => {
+    const matchesArray = text.match(config.jira.issueRegEx);
+    return matchesArray.length !== 1 ? null : matchesArray[0];
+}
+
+const requestToJira = (res, statusId, assignee) => {
+    const bodyData = `{
+        "transition": {
+            "id": ${statusId} 
+        },
+        ${assignee ? `"fields": {"assignee": {"name": "${assignee}"}}` : ''}
+    }`;
+
+    const options = {
         method: 'POST',
         url: `${config.jira.url}/rest/api/3/issue/${issueId}/transitions`,
         auth: { username: config.creds.username, password: config.creds.apiToken },
@@ -49,7 +96,7 @@ app.post('/toPrApproved', (req, res) => {
         body: bodyData
     };
 
-    request(options, function (error, response, body) {
+    request(options, (error, response, body) => {
         if (error) throw new Error(error);
         console.log(
             'Response: ' + response.statusCode + ' ' + response.statusMessage
@@ -57,6 +104,16 @@ app.post('/toPrApproved', (req, res) => {
         console.log(body);
         res.sendStatus(response.statusCode);
     });
-});
+}
+
+const findQaAssignee = (issueId) => {
+    const teamPrefix = issueId.split('-')[0];
+    const qaList = config.jira.qa[teamPrefix];
+    if (!qaList || qaList.length === 0) {
+        console.log(`Qa for team ${teamPrefix} was not found`);
+        return;
+    }
+    return qaList[Math.floor(Math.random() * qaList.length)];   
+}
 
 app.listen(config.port, () => console.log(`Example app listening on port ${config.port}!`));
